910dce17b7da89f819d694325159f3755658b332
DevCielo <136787904+DevCielo@users.noreply.github.com>
2024-07-15 10:17:12 +1000

    exported information and rendered tours

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,6 +1,39 @@
 const url = 'https://www.course-api.com/react-tours-project';
+import {useEffect, useState} from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setTours(data)
+        setIsLoading(false)
+      } catch (error) {
+        console.log('An error has occurred')
+        setIsLoading(false)
+      }
+    }
+    fetchData();
+  }, [])
+
+    if (isLoading) {
+      return <main>
+          <Loading />
+        </main>
+    }
+
+    return (
+      <main>
+        <Tours tours={tours} />
+      </main>
+    )
+  
 };
 export default App;
